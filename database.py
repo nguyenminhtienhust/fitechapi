65dab2d68cc6a49eda89600481e6bcb250627a38
TranNgocHa12 <tran.habk0605@gmail.com>
2024-03-07 16:43:24 +0700

    update

--- database.py
+++ database.py
@@ -490,12 +490,12 @@ def get_contact_by_name(name):
 		#return result[0]
 
 def get_contact_description(name):
-	conn = connect()
-	cursor = conn.cursor()
-	sql = ("SELECT * FROM suitecrm.contacts where id = %s")
-	cursor.execute(sql, (name,))
-	result = cursor.fetchone()
-	conn.close()
+	#conn = connect()
+	#cursor = conn.cursor()
+	#sql = ("SELECT * FROM suitecrm.contacts where id = %s")
+	#cursor.execute(sql, (name,))
+	#result = cursor.fetchone()
+	#conn.close()
 	return name
 	#if result is None:
 		#return ""
