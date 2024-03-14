dddf1e79c10abac1e6cbd755ec35c2839d068272
TranNgocHa12 <tran.habk0605@gmail.com>
2024-03-14 09:05:29 +0700

    update

--- database.py
+++ database.py
@@ -475,7 +475,7 @@ def add_email_addressed(id, email, email_cap):
 def get_contact_by_name(name):
 	conn = connect()
 	cursor = conn.cursor()
-	sql = ("SELECT * FROM suitecrm.contacts where last_name = %s and deleted = 1 and created_by = '1'")
+	sql = ("SELECT * FROM suitecrm.contacts where last_name = %s and created_by = '1'")
 	cursor.execute(sql, (name,))
 	result = cursor.fetchone()	
 	conn.close()
