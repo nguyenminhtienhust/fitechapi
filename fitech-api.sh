4c56cc8b186f094779088e5094879f8c669d031c
TranNgocHa12 <tran.habk0605@gmail.com>
2024-05-22 10:27:22 +0700

    update

--- fitech-api.sh
+++ fitech-api.sh
@@ -1,2 +1,2 @@
-#uvicorn main:app --port 9999 --host 0.0.0.0 --reload
-uvicorn.exe main:app --host=127.0.0.1 --port=5049
+uvicorn main:app --port 9999 --host 0.0.0.0 --reload
+#uvicorn.exe main:app --host=127.0.0.1 --port=5049
